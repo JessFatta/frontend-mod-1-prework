6ef2373fd09f11bcf0e578558f7cce1b29b3e025
Jessica Fatta <jessicafatta@gmail.com>
2021-09-13 13:30:30 -0400

    initial commit

---
+++ dataTypes.js
@@ -0,0 +1,12 @@
+console.log("99 bottles of pop on the wall...");
+console.log("leave the gun, take the cannoli");
+console.log(6*53);
+console.log(20/4);
+console.log(10%54);
+console.log("hello" === "Hello");
+console.log(3 != 4);
+console.log(4 <= 5);
+
+
+var name = "Harry Potter";
+console.log(name)
