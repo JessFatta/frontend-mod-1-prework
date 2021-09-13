ce7c94d3f22f3c736d9fd07b5bcf159a62b6be92
Jessica Fatta <jessicafatta@gmail.com>
2021-09-13 19:15:00 -0400

    please work

---
+++ interpolation.js
@@ -0,0 +1,14 @@
+var speedy = "quick red fox";
+var slowPoke = "lazy brown dog";
+
+console.log(`The ${speedy} jumped over the ${slowPoke}`);
+
+slowPoke = "tortoise";
+speedy = "hare";
+
+console.log(`In a predictable result, the ${slowPoke} beat the ${speedy}`);
+
+var lazy = "don't feel like cooking,";
+var dinner = "pizza";
+
+console.log(`I ${lazy} lets just order some ${dinner}`);
