cf53ce5bacb4a32ba783d4288cbae25cc3d68be8
Jessica Fatta <jessicafatta@gmail.com>
2021-09-13 14:07:23 -0400

    complete variable exercises

---
+++ variables.js
@@ -0,0 +1,24 @@
+var name = "Harry Potter";
+console.log(name);
+
+var enrolledStudents = 22 + 2;
+console.log(enrolledStudents);
+var order = "Harry Potter must not return to Hogwarts!";
+console.log(order);
+var firstName = "Jessica";
+console.log (firstName);
+var status = "is hungry";
+console.log (status);
+var numberOfPets = "currently just plants";
+console.log (numberOfPets);
+// I used string and numbers for the data types above because strings are used for any kind of text and the number data type is used for numbers
+var firstName = "Jess";
+console.log (firstName);
+var status = "is not hungry";
+console.log (status);
+var numberOfPets = ("0");
+console.log(numberOfPets);
+var healthySnacks = "6";
+console.log (healthySnacks);
+var junkFoodSnacks = "8";
+console.log (junkFoodSnacks);
