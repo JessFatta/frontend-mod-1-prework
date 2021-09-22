1c7994bf212e7cabb05062db4c510c0558db89c0
Jessica Fatta <jessicafatta@gmail.com>
2021-09-22 15:15:42 -0400

    added section4

--- section4/exercises/objects.js
+++ section4/exercises/objects.js
@@ -17,29 +17,61 @@ var foods = {
 };
 
 console.log(foods);
-// Notice that the variable name is foods, plural, because it has the ability to hold info about 
-// potentially many foods. That plural name indicates to another developer that the variable stores 
+// Notice that the variable name is foods, plural, because it has the ability to hold info about
+// potentially many foods. That plural name indicates to another developer that the variable stores
 // either an Array or an Object.
 
 //-------------------
 // Part 1: Zoo Animals
 //-------------------
 
-// YOU DO: Write code below that will declare a variable that stores an Object Literal of animal names and 
+// YOU DO: Write code below that will declare a variable that stores an Object Literal of animal names and
 // the number of that type of animal at the zoo.
+var animalNames = {
+  zebras: 11,
+  giraffes: 5,
+  penguins: 14,
+};
 
 
 // Using the zoo that you created above, log the value of the first item in
 // the Object
+console.log(animalNames.zebras);
 
 
 // Add an animal to the zoo Object, and log the updated Object.
+var animalNames = {
+  zebras: 11,
+  giraffes: 5,
+  penguins: 14,
+  monkeys: 100
+};
 
+console.log(animalNames.monkeys);
 
 // Change the value for at least one of your animal keys, and log the updated Object.
 
+var animalNames = {
+  zebras: 11,
+  giraffes: 5,
+  penguins: 14,
+  monkeys: 99
+};
+
+console.log(animalNames.monkeys);
+
 
 // Add another animal to the zoo Object, and log the updated Object.
+
+var animalNames = {
+  zebras: 11,
+  giraffes: 5,
+  penguins: 14,
+  monkeys: 99,
+  hippos: 3
+};
+
+console.log(animalNames.hippos);
 
 
 //-------------------
@@ -56,6 +88,16 @@ value should be some appropriate value for that key. Work to have at least 5 key
 Log your email object to the console.
 */
 
+var emailComponents = {
+  sender: "Turing",
+  subject: "Mod 0 Prework",
+  receiver: "Jessica Fatta",
+  date: "9/21/2021",
+  time: "6:45 pm"
+};
+
+console.log(emailComponents);
+
 //-------------------
 // Part 3: Many Emails - OPTIONAL EXTENSION
 //-------------------
@@ -70,7 +112,7 @@ In the arrays exercise, we gave the following example of an array of Instagram p
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 /*
-Frankly, that was a very simplified version of the Array the Instagram developers have 
+Frankly, that was a very simplified version of the Array the Instagram developers have
 written and work with. Still probably slightly simplified as we don't know what their code
 actually looks like, but it may look more like this:
 */
@@ -82,7 +124,7 @@ var posts = [
     timeStamp: "4:37 PM August 13, 2019",
     numberLikes: 0,
     comments: []
-  }, 
+  },
   {
     imageSrc: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -96,12 +138,31 @@ console.log(posts);
 console.log(posts[0]);
 
 /*
-The code snippet above shows an Array with 2 elements. Each element in an 
-Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK 
+The code snippet above shows an Array with 2 elements. Each element in an
+Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK
 a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 good to have some exposure before going into Mod 1.
 */
 
-// YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same 
+// YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same
 // key-value pairs you used in your email Object above.
 // Then, log the email Array to the console.
+
+var emailComponents = [
+  {
+    sender: "Turing",
+    subject: "Mod 0 Prework",
+    receiver: "Jessica Fatta",
+    date: "9/21/2021",
+    time: "6:45 pm"
+},
+{
+    sender: "Mom",
+    subject: "Dinner",
+    receiver: "Jeffrey Fatta",
+    date: "9/20/2021",
+    time: "4:45 pm"
+}
+]
+
+console.log(emailComponents);
